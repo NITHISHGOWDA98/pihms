cadf6862f7e86c7604299275c855ad4a68ef9d5d
NITHISHGOWDA98 <nithishgowda98@gmail.com>
2023-07-02 09:10:56 +0530

    fixed sidebar dropdown issues

--- src/NavBar/Main.js
+++ src/NavBar/Main.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import "./main.css";
 import { Outlet, useNavigate } from "react-router-dom";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFlaskVial } from "@fortawesome/free-solid-svg-icons";
 import {
+  faFlaskVial,
   faCalendar,
   faComment,
   faStethoscope,
@@ -25,309 +24,65 @@ import {
 
 export const Main = () => {
   const navigate = useNavigate();
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [dropdownStatus, setDropdownStatus] = useState({});
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const toggleDropdown = (moduleName) => {
+    setDropdownStatus((prevStatus) => ({
+      ...prevStatus,
+      [moduleName]: !prevStatus[moduleName],
+    }));
   };
-  const handleNavigation = (path) => {
+
+  const handleNavigation = (path, moduleName) => {
+    toggleDropdown(moduleName);
     navigate(path);
   };
+
+  const renderDropdownContent = (moduleName) => {
+    return (
+      dropdownStatus[moduleName] && (
+        <div className="dropdown-content">
+          <p>This is the dropdown content.</p>
+        </div>
+      )
+    );
+  };
+
+  const Module = (path, moduleName, icon) => (
+    <div className="modules">
+      <p className="modules-p" onClick={() => handleNavigation(path, moduleName)}>
+        <span className="modules-icon">
+          <FontAwesomeIcon icon={icon} />
+        </span>
+        <span className="modules-title">{moduleName}</span>
+        <span className="dropdown-icon">
+          <FontAwesomeIcon icon={dropdownStatus[moduleName] ? faChevronUp : faChevronDown} />
+        </span>
+      </p>
+      {renderDropdownContent(moduleName)}
+    </div>
+  );
 
   return (
     <div className="maincontainer">
       <div className="leftcontainer">
         <div className="leftcontainerheader">PI-HMS</div>
         <div className="modulelist">
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/appointment")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faCalendar} />
-              </span>
-              <span className="modules-title">Appointment</span>
-
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/communication")}
-            >
-              <span className="modules-icon">
-              <FontAwesomeIcon icon={faComment}  />
-              </span>
-              <span className="modules-title"> Communication </span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/diagnostic")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faFlaskVial} />
-              </span>
-              <span className="modules-title">Diagnostic</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/doctorconsultation")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faStethoscope} />
-              </span>
-              <span className="modules-title">Consultation</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/finance")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faMoneyBillWave} />
-              </span>
-              <span className="modules-title">Finance</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/general")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faCog} />
-              </span>
-              <span className="modules-title">General</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/housekeeping")}
-            >
-              <span className="modules-icon">
-                {<FontAwesomeIcon icon={faBroom} />}
-              </span>
-              <span className="modules-title">HouseKeeping</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-          </div>
-
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/inpatient")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faHospital} />
-              </span>
-              <span className="modules-title">InPatient</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/inventory")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faBoxOpen} />
-              </span>
-              <span className="modules-title">Inventory</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/masters")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faClipboardList} />
-              </span>
-              <span className="modules-title">Masters</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/opbilling")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faMoneyBill} />
-              </span>
-
-              <span className="modules-title" onClick={toggleDropdown}>
-                OpBilling 
-              </span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-              
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/patient")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faUser} />
-              </span>
-              <span className="modules-title">Patient</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/pharmacy")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faPrescriptionBottleAlt} />
-              </span>
-              <span className="modules-title">Pharmacy</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p
-              className="modules-p"
-              onClick={() => handleNavigation("/security")}
-            >
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faShieldAlt} />
-              </span>
-              <span className="modules-title">Security</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
-          <div className="modules">
-            <p className="modules-p" onClick={() => handleNavigation("/staff")}>
-              <span className="modules-icon">
-                <FontAwesomeIcon icon={faUsers} />
-              </span>
-              <span className="modules-title">Staff</span>
-              <span className="dropdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={isDropdownOpen ? faChevronUp : faChevronDown} />
-              </span>
-            </p>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <p>This is the dropdown content.</p>
-              </div>
-            )}
-          </div>
+          {Module("/appointment", "Appointment", faCalendar)}
+          {Module("/communication", "Communication", faComment)}
+          {Module("/diagnostic", "Diagnostic", faFlaskVial)}
+          {Module("/doctorconsultation", "Consultation", faStethoscope)}
+          {Module("/finance", "Finance", faMoneyBillWave)}
+          {Module("/general", "General", faCog)}
+          {Module("/housekeeping", "HouseKeeping", faBroom)}
+          {Module("/inpatient", "InPatient", faHospital)}
+          {Module("/inventory", "Inventory", faBoxOpen)}
+          {Module("/masters", "Masters", faClipboardList)}
+          {Module("/opbilling", "OpBilling", faMoneyBill)}
+          {Module("/patient", "Patient", faUser)}
+          {Module("/pharmacy", "Pharmacy", faPrescriptionBottleAlt)}
+          {Module("/security", "Security", faShieldAlt)}
+          {Module("/staff", "Staff", faUsers)}
         </div>
       </div>
       <div className="rightcontainer">
