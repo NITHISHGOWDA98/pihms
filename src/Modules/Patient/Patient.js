0b0854b7525bc7ee9b892ca9bb7978a24180874c
rpncsystems <135303149+rpncsystems@users.noreply.github.com>
2023-07-05 17:14:04 +0530

    added 2 apis

--- src/Modules/Patient/Patient.js
+++ src/Modules/Patient/Patient.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./patient.css";
-
+import { faHistory, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Header } from "../../NavBar/Header";
 
 export const Patient = () => {
   const [patients, setPatients] = useState([]);
+  const [selectedPatient, setSelectedPatient] = useState(null);
   const [inputPatientID, setInputPatientID] = useState("");
+  const [isSecondAPIDisplayed, setIsSecondAPIDisplayed] = useState(false);
+  const [selectedPatientData, setSelectedPatientData] = useState(null);
 
-  const fetchPatientDetails = async () => {
-    const apiUrl = "http://ganga.pihms.co.in/Patient/get_PatientDetail";
+  useEffect(() => {
+    fetchPatientList();
+  }, []);
+
+  const fetchPatientList = async () => {
+    const apiUrl = "http://ganga.pihms.co.in/Patient/get_PatientList";
 
     try {
       const requestBody = {
-        m_PatientID: inputPatientID,
+        m_FilterOptions: {
+          m_Type: 5,
+          m_Limit: "100",
+          m_OrderBy: "id",
+        },
       };
 
       const response = await fetch(apiUrl, {
@@ -25,13 +37,48 @@ export const Patient = () => {
 
       if (response.ok) {
         const data = await response.json();
-        console.log("Fetched data:", data);
-        setPatients([data.m_Patient]);
+        console.log("Fetched patient list:", data);
+        setPatients(data.ml_Patient);
       } else {
-        console.error("Failed to fetch patient details.");
+        console.error("Failed to fetch patient list.");
       }
     } catch (error) {
-      console.error("Error occurred while fetching patient details:", error);
+      console.error("Error occurred while fetching patient list:", error);
+    }
+  };
+
+  const fetchPatientDetails = async () => {
+    if (inputPatientID) {
+      const apiUrl = "http://ganga.pihms.co.in/Patient/get_PatientDetail";
+
+      try {
+        const requestBody = {
+          m_PatientID: inputPatientID,
+        };
+
+        const response = await fetch(apiUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Fetched patient details:", data);
+          setSelectedPatientData(data.m_Patient);
+          setIsSecondAPIDisplayed(true);
+        } else {
+          console.error("Failed to fetch patient details.");
+        }
+      } catch (error) {
+        console.error("Error occurred while fetching patient details:", error);
+      }
+    } else {
+      // No input provided, display first API data
+      setSelectedPatient(null && inputPatientID === ' ');
+      setIsSecondAPIDisplayed(false);
     }
   };
 
@@ -47,24 +94,37 @@ export const Patient = () => {
   return (
     <div className="paintentmaincontianer">
       <Header />
-
-      <form className="patientform" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputPatientID}
-          onChange={handleInputChange}
-          placeholder="Enter Patient ID"
-          className="patientinput"
-        />
-        <button type="submit" className="patientinputbutton">Submit</button>
-      </form>
+      <div className="belowheader">
+        <form className="patientform" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={inputPatientID}
+            onChange={handleInputChange}
+            placeholder="Enter Patient ID"
+            className="patientinput"
+          />
+          <button type="submit" className="patientinputbutton">
+            Search
+          </button>
+        </form>
+        <div className="belowheaderfilter">
+          <div className="filter">
+            <FontAwesomeIcon icon={faHistory} style={{ marginRight: "10px" }} />{" "}
+            Recent{" "}
+            <FontAwesomeIcon
+              icon={faAngleDown}
+              style={{ marginLeft: "10px", marginTop: "4px" }}
+            />
+          </div>
+        </div>
+      </div>
 
       <div className="patientcontent">
         <div className="table-wrapper">
           <table className="table">
             <thead>
               <tr className="table-header">
-                <th scope="col">Sl No</th>
+                <th scope="col">Id</th>
                 <th scope="col">Patient No</th>
                 <th scope="col">Patient Name</th>
                 <th scope="col">Display Name</th>
@@ -73,21 +133,43 @@ export const Patient = () => {
               </tr>
             </thead>
             <tbody>
-              {patients.map((patient, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
+              {!isSecondAPIDisplayed && patients.map((patient) => (
+                <tr
+                  key={patient.id}
+                  onClick={() => setSelectedPatient(patient)}
+                >
+                  <td>{patient.id}</td>
                   <td>{patient.patientNumber}</td>
                   <td>{patient.firstName}</td>
                   <td>{patient.displayName}</td>
                   <td>{patient.gender}</td>
                   <td>{patient.dateOfBirth}</td>
-                  <td>{/* Action buttons here */}</td>
                 </tr>
               ))}
+
+              {isSecondAPIDisplayed && selectedPatientData && (
+                <tr>
+                  <td>{selectedPatientData.id}</td>
+                  <td>{selectedPatientData.patientNumber}</td>
+                  <td>{selectedPatientData.firstName}</td>
+                  <td>{selectedPatientData.displayName}</td>
+                  <td>{selectedPatientData.gender}</td>
+                  <td>{selectedPatientData.dateOfBirth}</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
       </div>
+
+      {isSecondAPIDisplayed && selectedPatient && (
+        <div className="patientdetails">
+          <h2>Patient Details</h2>
+          <div>Selected Patient ID: {selectedPatient.id}</div>
+          <div>Selected Patient Name: {selectedPatient.displayName}</div>
+          {/* Add more details as per your requirement */}
+        </div>
+      )}
     </div>
   );
 };
